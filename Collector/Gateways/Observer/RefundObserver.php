055f7120503c44309f932dc6ebfb06856937e7db
Henrik Löfström <Henrik@geborek.com>
2018-01-04 11:27:21 +0100

    initial upload

---
+++ Collector/Gateways/Observer/RefundObserver.php
@@ -0,0 +1,16 @@
+<?php
+namespace Collector\Gateways\Observer;
+
+use Magento\Framework\DataObject;
+use Magento\Framework\Encryption\EncryptorInterface;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Payment\Observer\AbstractDataAssignObserver;
+use Magento\Quote\Api\Data\PaymentInterface;
+use Magento\Payment\Model\InfoInterface;
+
+class RefundObserver extends AbstractDataAssignObserver{
+    public function execute(Observer $observer){
+		
+    }
+}
---
+++ Collector/Iframe/view/frontend/web/js/checkout.js
@@ -0,0 +1,12 @@
+define([
+	'jquery'
+], function ($, collectorajax) {
+	document.addEventListener('collectorCheckoutCustomerUpdated', function(event){
+	});
+	document.addEventListener('collectorCheckoutOrderValidationFailed', function(event){
+	});
+	document.addEventListener('collectorCheckoutLocked', function(event){
+	});
+	document.addEventListener('collectorCheckoutUnlocked', function(event){
+	});
+});
